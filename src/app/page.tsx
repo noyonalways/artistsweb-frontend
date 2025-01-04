a48921a06bc30d88c42961be81c4716e24c18ae2
noyonalways <noyonrahman2003@gmail.com>
2025-01-04 18:11:04 +0600

    feat: dev branch created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="min-h-svh flex items-center justify-center bg-background">
+      <div className="container">
+        <h1 className="text-[12vw] md:text-[6.5vw] lg:text-[7.75vw] lg:leading-tight font-semibold font-onest">
+          Crafting{" "}
+          <span className="text-transparent bg-gradient-to-r from-foreground via-primary to-foreground bg-clip-text animate-heroTextGradient bg-[length:200%_auto]">
+            Digital{" "}
+          </span>
+          Experiences
+        </h1>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1723px",
+      },
+      padding: {
+        DEFAULT: ".5rem",
+        sm: "0rem",
+      },
+    },
     extend: {
       colors: {
+        primary: "#545cff",
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        onest: ["var(--font-onest)"],
+      },
+      animation: {
+        heroTextGradient: "heroTextGradient 5s linear infinite",
+      },
+      keyframes: {
+        heroTextGradient: {
+          "0%": { backgroundPosition: "0% center" },
+          "100%": { backgroundPosition: "-200% center" },
+        },
       },
     },
   },
